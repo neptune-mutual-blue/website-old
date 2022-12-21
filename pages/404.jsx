d889ea70d77f98ce1780a6972497e5a2a3d11e7c
acharyarupak391 <acharyarupak391@gmail.com>
2022-12-21 13:39:53 +0545

    reverted meta tag changes in language files

--- pages/404.jsx
+++ pages/404.jsx
@@ -36,18 +36,18 @@ export default function NotFoundPage () {
         <link rel='icon' href='/favicon.ico' />
 
         <meta property='og:type' content='website' />
-        <meta property='og:title' content={t('Could Not Find the Page / Neptune Mutual')} />
-        <meta property='og:description' content={t('The page you requested either not exist or has been deleted')} />
-        <meta property='og:image' content={getFQDN('/assets/images/meta/og/home.png')} />
+        <meta property='og:title' content={t('META_OG_TITLE')} />
+        <meta property='og:description' content={t('META_OG_DESCRIPTION')} />
+        <meta property='og:image' content={getFQDN(t('META_OG_IMAGE'))} />
         <meta property='og:locale' content={router.locale} />
         <meta property='og:url' content={router.asPath} />
         <meta property='twitter:site' content='@neptunemutual' />
         <meta property='twitter:creator' content='@neptunemutual' />
         <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:description' content={t('The page you requested either not exist or has been deleted')} />
-        <meta property='twitter:title' content={t('Could Not Find the Page / Neptune Mutual')} />
-        <meta property='twitter:image' content={getFQDN('/assets/images/meta/og/home.png')} />
-        <meta property='twitter:image:alt' content={t('Could Not Find the Page Banner')} />
+        <meta property='twitter:description' content={t('META_TWITTER_DESCRIPTION')} />
+        <meta property='twitter:title' content={t('META_TWITTER_TITLE')} />
+        <meta property='twitter:image' content={getFQDN(t('META_TWITTER_IMAGE'))} />
+        <meta property='twitter:image:alt' content={t('META_TWITTER_IMAGE_ALT')} />
 
         <meta name='robots' content='noindex' />
       </Head>
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -42,18 +42,18 @@ export default function BlogPage (props) {
         <link rel='icon' href='/favicon.ico' />
 
         <meta property='og:type' content='website' />
-        <meta property='og:title' content={t('Neptune Mutual Decentralized Insurance Blog')} />
-        <meta property='og:description' content={t('Find out our most current updates, recent instances involving decentralized insurance news, hacks, rug pulls, and exploits.')} />
-        <meta property='og:image' content={getFQDN('/assets/images/meta/og/blog.png')} />
+        <meta property='og:title' content={t('META_OG_TITLE')} />
+        <meta property='og:description' content={t('META_OG_DESCRIPTION')} />
+        <meta property='og:image' content={getFQDN(t('META_OG_IMAGE'))} />
         <meta property='og:locale' content={router.locale} />
         <meta property='og:url' content={router.asPath} />
         <meta property='twitter:site' content='@neptunemutual' />
         <meta property='twitter:creator' content='@neptunemutual' />
         <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:description' content={t('Find out our most current updates, recent instances involving decentralized insurance news, hacks, rug pulls, and exploits.')} />
-        <meta property='twitter:title' content={t('Neptune Mutual Decentralized Insurance Blog')} />
-        <meta property='twitter:image' content={getFQDN('/assets/images/meta/og/blog.png')} />
-        <meta property='twitter:image:alt' content={t('Neptune Mutual Blog Open Graph Banner')} />
+        <meta property='twitter:description' content={t('META_TWITTER_DESCRIPTION')} />
+        <meta property='twitter:title' content={t('META_TWITTER_TITLE')} />
+        <meta property='twitter:image' content={getFQDN(t('META_TWITTER_IMAGE'))} />
+        <meta property='twitter:image:alt' content={t('META_TWITTER_IMAGE_ALT')} />
       </Head>
 
       <main>
